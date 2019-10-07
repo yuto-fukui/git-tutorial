a1f9fa817f8aac2aff62c270460505548912f949
fukui <yuto-fukui-3423@docomo.ne.jp>
2019-10-07 10:11:28 +0900

    Add mkfizz

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,14 @@
 #!/usr/bin/env ruby
-print("helloWorld!\n")
+(1..100).each {|i|
+  if i%3==0 and i%5==0
+    print "FizzBuzz\n"
+  elsif i%3==0
+    print "Fizz\n"
+  elsif i%5==0
+    print "Buzz\n"
+  else
+    printf("%d\n",i)
+  end
+}
+  
+  
